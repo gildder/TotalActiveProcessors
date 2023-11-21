512d767d038774563b41628d3058c74e48bcfad4
Gildder <gilberet@hotmail.com>
2023-11-21 19:08:21 -0400

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace ProcessorInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int processorCount = Environment.ProcessorCount;
+            Console.WriteLine($"Número total de procesadores lógicos: {processorCount}");
+
+            foreach (var item in new System.Management.ManagementObjectSearcher("Select * from Win32_Processor").Get())
+            {
+                Console.WriteLine($"Nombre del procesador: {item["Name"]}");
+                Console.WriteLine($"Descripción: {item["Description"]}");
+                Console.WriteLine($"ID del procesador: {item["ProcessorId"]}");
+                Console.WriteLine($"Velocidad del reloj: {item["MaxClockSpeed"]} MHz");
+            }
+
+            // Pausa el programa por 15 segundos
+            Thread.Sleep(15000);
+        }
+    }
+}
